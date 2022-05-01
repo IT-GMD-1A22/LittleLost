3eeaabaf42891cd7a7b2292d525f4c38bf72ad89
krogh13 <krogh@hotmail.dk>
2022-05-01 21:35:23 +0200

    minor changes

--- Assets/Scripts/NK/NarratorAudioTrigger.cs
+++ Assets/Scripts/NK/NarratorAudioTrigger.cs
@@ -9,14 +9,7 @@ public class NarratorAudioTrigger : MonoBehaviour
 {
     [SerializeField] private int colliderId = 0;
     [SerializeField] private bool playOnce = true;
-    [SerializeField] private bool disablePlayer = true;
-
-    private PlayerController _playerController;
-
-    private void Start()
-    {
-        _playerController = GetComponent<PlayerController>();
-    }
+    [SerializeField] private bool playMultiple = true;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,11 +18,11 @@ public class NarratorAudioTrigger : MonoBehaviour
             if (playOnce)
             {
                 print("Play once");
-                // _playerController.enabled = false;
                 NarratorSoundManager.Instance.PlaySoundClip(colliderId);
-                // _playerController.enabled = true;
+                playOnce = false;
             }
-            else
+
+            if (playMultiple)
             {
                 NarratorSoundManager.Instance.PlaySoundClip(colliderId);
                 print("Play mult times");
